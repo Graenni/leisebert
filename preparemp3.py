6ecb955d9080f05134e2e0767a8671702e0070cb
Michael Schmutz <m.schmutz@sunrise.ch>
2020-07-16 21:54:43 +0200

    implemented contents file

--- preparemp3.py
+++ preparemp3.py
@@ -1,18 +1,39 @@
 import os
 import eyed3
 
-for folder in range(1, 9):
-    path = "./" + str(folder) + '/'
-    if not os.path.isdir(path):
-        os.mkdir(path)
+# TODO output tracklist or covers to sd card
 
-    files = os.listdir(path)
-    for file in files:
-        filename = path + file
-        if file.endswith(".mp3") or file.endswith(".MP3"):
-            print(file)
-            audiofile = eyed3.load(filename)
-            newfile = path + f'{audiofile.tag.track_num[0]:03d}' + '.mp3'
-            os.rename(filename, newfile)
-        else:
-            os.remove(filename)
+colors = ['white', 'red', 'transparent', 'yellow', 'black', 'green', 'grey', 'blue']
+
+with open('contents.txt', 'w') as outfile:
+    outfile.write('L E I S E B E R T   C O N T E N T S\n')
+    outfile.write('-----------------------------------\n')
+    for folder in range(1, 9):
+        fld = f'{folder:02d}'
+        path = "./" + fld + '/'
+        if not os.path.isdir(path):
+            os.mkdir(path)
+        files = os.listdir(path)
+        tracks = []
+        album = ''
+        for fi, file in enumerate(files):
+            filename = path + file
+            if file.endswith(".mp3") or file.endswith(".MP3"):
+                print(file)
+                audiofile = eyed3.load(filename)
+                if fi == 0:
+                    album = audiofile.tag.album
+                track = f'{audiofile.tag.track_num[0]:03d}'
+                tracks.append((audiofile.tag.track_num[0], audiofile.tag.title))
+                
+                newfile = path + track + '.mp3'
+                os.rename(filename, newfile)
+            else:
+                os.remove(filename)
+
+        # write contents file
+        tracks.sort(key=lambda x: x[0])
+        outfile.write('FOLDER ' + fld + ' (' + colors[folder-1] + '): ' + album + '\n')                   
+        for tr in tracks:
+            outfile.write('     ' + f'{tr[0]:03d}' + ': ' + tr[1] + '\n')
+        outfile.write('-----------------------------------\n')
